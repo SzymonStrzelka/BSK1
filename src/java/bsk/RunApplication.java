3d6cc095826890188808b1a5fe10798bf993f72b
SzymonStrzelka <szymek.strzelka@gmail.com>
2018-03-10 23:28:31 +0100

    implement basic interface

---
+++ src/java/bsk/RunApplication.java
@@ -0,0 +1,28 @@
+package bsk;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class RunApplication extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = Paths.get("src/resources/fxml/MainWindow.fxml").toUri().toURL();
+        Pane mainWindow = FXMLLoader.load(url);
+        Scene scene = new Scene(mainWindow);
+
+        primaryStage.setTitle("Data encryption application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
